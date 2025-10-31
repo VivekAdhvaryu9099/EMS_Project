223db461c7e31c3675dde3d7de9de45a0e68d704
VivekAdhvaryu9099 <adhvaryuvivek45@gmail.com>
2025-10-31 13:27:54 +0530

    Add files via upload

---
+++ Server/db/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+ const connectToDb = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+    } catch (error) {
+        console.log(error);
+        
+    }
+} 
+
+export default connectToDb
